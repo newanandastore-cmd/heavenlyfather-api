077d4018993a40338c1c44ef62b1b9d0380d008a
newanandastore-cmd <newanandastore@gmail.com>
2026-05-30 17:04:31 +0530

    Update bible.js

--- api/bible.js
+++ api/bible.js
@@ -1,12 +1,14 @@
 export default async function handler(req, res) {
+
   const { reference = "John 3:16" } = req.query;
 
   try {
+
     const response = await fetch(
       `https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/passages/${encodeURIComponent(reference)}`,
       {
         headers: {
-          "api-key": "XpyMeeoLPX0_-mvUg7wub"
+          "api-key": process.env.BIBLE_API_KEY
         }
       }
     );
@@ -16,8 +18,10 @@ export default async function handler(req, res) {
     res.status(200).json(data);
 
   } catch (error) {
+
     res.status(500).json({
       error: error.message
     });
+
   }
 }
